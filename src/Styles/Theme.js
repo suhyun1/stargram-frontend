d9a2e0a32616bbea204ee34ccd4edbb5cc982fab
suhyun1 <suhyun7079@gmail.com>
2020-03-13 00:09:17 +0900

    style: Add globalstyles and theme

---
+++ src/Styles/Theme.js
@@ -0,0 +1,17 @@
+const BOX_BORDER = "1px solid #e6e6e6";
+const BORDER_RADIUS = "4px";
+export default {
+  bgColor: "#ecf0f1",
+  blackColor: "#262626",
+  darkGrayColor: "#999",
+  lightGrayColor: "#c7c7c7",
+  redColor: "#ED4956",
+  blueColor: "#3897f0",
+  darkBlueColor: "#273c75",
+  boxBorder: "1px solid #e6e6e6",
+  borderRadius: "4px",
+  whiteBox: `${BOX_BORDER};
+            ${BORDER_RADIUS};
+            background-color:white;
+            `
+};
