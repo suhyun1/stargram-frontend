81c3aa60a35263f10aae6f6caa9b2501d3c65f37
suhyun1 <suhyun7079@gmail.com>
2020-03-17 23:52:29 +0900

    feat(auth): Add toast notifications

--- src/Routes/Auth/AuthContainer.js
+++ src/Routes/Auth/AuthContainer.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import AuthPresenter from "./AuthPresenter";
 import useInput from "../../Hooks/useInput";
 import { useMutation } from "react-apollo-hooks";
-import { LOG_IN } from "./AuthQuery";
+import { LOG_IN, CREATE_ACCOUNT } from "./AuthQuery";
+import { toast } from "react-toastify";
 
 export default () => {
     const [action, setAction] = useState("logIn");
@@ -11,16 +12,45 @@ export default () => {
     const lastName = useInput("");
     const email = useInput("");
     const [requestSecret] = useMutation(LOG_IN, { 
-        variables: { email: email.value } 
+        update: (_, { data }) => {//mutation의 result 얻음 (data)
+            const {requestSecret} = data;
+            if(!requestSecret){
+                toast.error("You don't have an account yet, create ID!");
+                setTimeout(() => setAction("signUp"),3000);
+            }
+        }, 
+        variables: { email: email.value }
+    });
+    const [createAccount] = useMutation(CREATE_ACCOUNT, {
+        variables: {
+            email: email.value,
+            username: username.value,
+            firstName: firstName.value,
+            lastName: lastName.value
+        }
     });
 
-
-    const onLogin = (e) => {
+    const onSubmit = (e) => {
         e.preventDefault();
-        console.log(email);
-        if(email !== ""){
-            requestSecret();
+        if(action ==="logIn"){
+            if (email.value !== "") {
+              requestSecret();
+            }else{
+                toast.error("Email is required");
+            }
+        }else if(action ==="signUp"){
+            if (
+              email.value !== "" &&
+              username.value !== "" &&
+              firstName.value !== "" &&
+              lastName.value !== ""
+            ) {
+                createAccount();
+            }else{
+                toast.error("All field are required");
+            }
         }
+        
     }
     return (
         <AuthPresenter 
@@ -30,7 +60,7 @@ export default () => {
             firstName={firstName}
             lastName={lastName}
             email={email}
-            onLogin={onLogin}
+            onLogin={onSubmit}
         />
 
 
