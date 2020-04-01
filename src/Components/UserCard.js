304d71e55329798007696259d48eb4f9d838bc2d
suhyun1 <suhyun7079@gmail.com>
2020-04-01 20:48:35 +0900

    feat(follow): Add followButton component

--- src/Components/UserCard.js
+++ src/Components/UserCard.js
@@ -3,8 +3,8 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 import Avatar from "./Avatar";
 import FatText from "./FatText";
-import Button from "./Button";
 import { Link } from "react-router-dom";
+import FollowButton from "./FollowButton"
 
 const Card = styled.div`
     ${props => props.theme.whiteBox};
@@ -23,13 +23,13 @@ const ELink = styled(Link)`
     color: inherit;
     margin-bottom: 10px;
 `;
-const UserCard = ({username, isFollowing, url, isSelf}) => (
+const UserCard = ({id, username, isFollowing, url, isSelf}) => (
     <Card>
         <EAvatar url={url} size={"md"} />
         <ELink to={`/${username}`}>
             <FatText text={username} />
         </ELink>
-        { !isSelf && <Button text={isFollowing ? "Unfollow" : "Follow"} /> }
+        { !isSelf && <FollowButton id={id} isFollowing={isFollowing}/>}
     </Card >
 
 );
@@ -37,6 +37,7 @@ const UserCard = ({username, isFollowing, url, isSelf}) => (
 
 
 UserCard.propTypes = {
+    id: PropTypes.string.isRequired,
     username: PropTypes.string.isRequired,
     isFollowing: PropTypes.string.isRequired,
     url: PropTypes.string.isRequired,
