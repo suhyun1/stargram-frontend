c32b0e0ce0aca9e289ed42586de411266124ea09
suhyun1 <suhyun7079@gmail.com>
2020-04-03 23:00:18 +0900

    fix: Add uid=197609(정수현) gid=197121 groups=197121 to searchPost's return value:

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -59,6 +59,7 @@ const SearchPresenter = ({searchTerm, loading, data}) =>{
                     ):(
                         data.searchPost.map(post => (
                             <SquarePost 
+                                key={post.id}
                                 likeCount={post.likeCount}
                                 commentCount={post.commentCount} 
                                 file={post.files[0]} 
