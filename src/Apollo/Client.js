95deec92e2183177f3b47127ca8ca7328525f482
suhyun1 <suhyun7079@gmail.com>
2020-06-26 23:06:00 +0900

    chore: Check process.env.NODE_ENV

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -1,6 +1,6 @@
 import ApolloClient from "apollo-boost";
 import { defaults, resolvers } from "./LocalState";
-
+console.log(process.env.NODE_ENV);
 export default new ApolloClient({
   uri:
     process.env.NODE_ENV === "development"
